8730e911613bcac73b0dd0ea6ea2304ce7ae8f1a
Rafael Rosa <152110648+rafaelsilvdarosa@users.noreply.github.com>
2024-11-06 09:56:36 -0300

    adicionado if para informar se o chute é maior ou menor que o número.

--- main.ts
+++ main.ts
@@ -2,12 +2,17 @@ let chute = 0
 let numeroSecreto = 0
 game.showLongText("Bem vindo ao jogo do número secreto!", DialogLayout.Bottom)
 forever(function () {
-    numeroSecreto = randint(0, 10)
+    numeroSecreto = randint(1, 10)
     chute = game.askForNumber("Escolha um número de 1 a 20")
     if (chute == numeroSecreto) {
         game.splash("Você acertou!!! Parabéns!")
         game.gameOver(true)
     } else {
-        game.splash("Não foi dessa vez :(, tente novamente!")
+        game.splash("Não foi dessa vez :(")
+    }
+    if (chute < numeroSecreto) {
+        game.splash("O número secreto é maior.")
+    } else {
+        game.splash("O número secreto é menor.")
     }
 })
